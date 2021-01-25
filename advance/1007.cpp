b529e69bd93b30731b947fd4bd51203ad9d569c4
Saltsuica <740050093@qq.com>
2021-01-26 00:15:05 +0800

    Need to opt

--- advance/1007.cpp
+++ advance/1007.cpp
@@ -2,6 +2,13 @@
 #include <queue>
 using namespace std;
 
+int getSum(const int a[], int begin_idx, int end_idx) {
+    int sum = 0;
+    for (int i = begin_idx; i <= end_idx; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -15,19 +22,24 @@ int main(int argc, char const *argv[])
 
     int win_left = 0, win_right = 0;
 
-    for (int i = 0; i < n; i++) {
-        //TO-DO: silding window
+    int win_size = n, max_sum = 0;
+    bool areAllNagetive = false;
+    while (win_size >= 0) {
+        int begin_idx = 0, tmp_sum;
+        while (begin_idx + win_size < n) {
+            tmp_sum = getSum(data, begin_idx, begin_idx + win_size);
+            if (tmp_sum > max_sum) {
+                max_sum = tmp_sum;
+                win_left = begin_idx;
+                win_right = begin_idx + win_size;
+            }
+            begin_idx++;
+        }
+        win_size--;
     }
 
-    int sum = 0;
-    for (int i = win_left; i <= win_right; i++) {
-        sum += data[i];
-    }
-
-    cout << sum;
-    for (int i = win_left; i <= win_right; i++) {
-        cout << data[i];
-    }
+    cout << max_sum;
+    cout << " " << data[win_left] << " " << data[win_right];
 
     return 0;
 }
