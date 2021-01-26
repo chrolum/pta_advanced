c75ccc370962d06b6be81ced493bfd65d6887acc
Saltsuica <740050093@qq.com>
2021-01-26 15:00:12 +0800

    Brute force

--- advance/1007.cpp
+++ advance/1007.cpp
@@ -22,20 +22,33 @@ int main(int argc, char const *argv[])
 
     int win_left = 0, win_right = 0;
 
-    int win_size = n, max_sum = 0;
-    bool areAllNagetive = false;
-    while (win_size >= 0) {
+    int win_size = 0, max_sum = 0;
+    bool areAllNagetive = true;
+    while (win_size <= n-1) {
         int begin_idx = 0, tmp_sum;
         while (begin_idx + win_size < n) {
+            if (data[begin_idx] >= 0)
+                areAllNagetive = false;
             tmp_sum = getSum(data, begin_idx, begin_idx + win_size);
             if (tmp_sum > max_sum) {
                 max_sum = tmp_sum;
                 win_left = begin_idx;
-                win_right = begin_idx + win_size;
+                win_right = win_left + win_size;
+            }
+
+            if (tmp_sum == max_sum && begin_idx < win_left) {
+                win_left = begin_idx;
+                win_right = win_left + win_size;
             }
             begin_idx++;
         }
-        win_size--;
+        win_size++;
+    }
+
+    if (areAllNagetive) {
+        max_sum = 0;
+        win_left = 0;
+        win_right = n-1;
     }
 
     cout << max_sum;
