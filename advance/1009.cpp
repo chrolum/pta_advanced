7f4b9a75b40fba618f13ef5baaea6fbef6072c11
Saltsuica <740050093@qq.com>
2021-01-30 17:16:39 +0800

    1009 20/25

---
+++ advance/1009.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <iostream>
+#include <map>
+using namespace std;
+
+void read_ploy(int n, map<int, double>& ploy) {
+    int e;
+    double c;
+    for (int i = 0; i < n; i++) {
+        cin >> e >> c;
+        ploy[e] = c;
+    }
+}
+
+bool isNormal(double a) {
+    return (a < 10) && (a > -10);
+}
+
+void add_normalize(int e, double c, map<int, double>& ploy) {
+    if (ploy.count(e) == 0) {
+        ploy[e] = c;
+    }
+    else { // will be continous carry in
+        bool hasCarried = true;
+        while (hasCarried) {
+            c += ploy[e];
+            if (!isNormal(c)) { // has carry
+                hasCarried = true;
+                c = c / 10;
+                ploy[e] = c;
+                c = 1; //carry one
+                e++;
+            }
+            else {
+                hasCarried = false;
+                ploy[e] = c;
+            }    
+        }
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    map<int, double> ploy1;
+    map<int, double> ploy2;
+    map<int, double> res;
+
+    int n;
+    cin >> n;
+    read_ploy(n, ploy1);
+    cin >> n;
+    read_ploy(n, ploy2);
+
+    //product
+    int tmp_e;
+    double tmp_c, normal_c, carry;
+    for (auto item1 : ploy1) {
+        for (auto item2 : ploy2) {
+            tmp_e = item1.first + item2.first;
+            tmp_c = item1.second * item2.second;
+            normal_c = tmp_c;
+            carry = 0;
+
+            //normalize the tmp coff
+            while (!isNormal(normal_c)) {
+                normal_c = normal_c / 10;
+                carry++;
+            }
+            tmp_e += carry;
+            
+            add_normalize(tmp_e, tmp_c, res);
+        }
+    }
+
+    // print res
+    cout << res.size();
+
+    for (auto item = res.rbegin(); item != res.rend(); item++) {
+        cout << " " << item->first;
+        printf(" %.1f", item->second);
+    }
+    return 0;
+}
+
